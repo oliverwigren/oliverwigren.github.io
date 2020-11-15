7e3518ae13a61fa10ee50fb2eca8c2f8d7bfc2a7
Burksson <57063539+Burksson@users.noreply.github.com>
2020-11-15 11:04:01 +0100

    Create random.js

---
+++ random.js
@@ -0,0 +1,41 @@
+var X = Math.floor(Math.random() * 10); 
+  if(X===1) {
+     document.getElementById("footer").style.backgroundColor = "red";
+    document.getElementById("activenav").style.backgroundColor = "red";
+     }
+  if(X===2) {
+  document.getElementById("footer").style.backgroundColor = "teal";
+  document.getElementById("activenav").style.backgroundColor = "teal";
+  }
+  if(X===3) {
+ document.getElementById("footer").style.backgroundColor = "orange";
+ document.getElementById("activenav").style.backgroundColor = "orange";
+  }
+  if(X===4) {
+ document.getElementById("footer").style.backgroundColor = "steelblue";
+ document.getElementById("activenav").style.backgroundColor = "steelblue";
+  }
+  if(X===5) {
+ document.getElementById("footer").style.backgroundColor = "pink";
+ document.getElementById("activenav").style.backgroundColor = "pink";
+  }
+  if(X===6) {
+ document.getElementById("footer").style.backgroundColor = "palevioletred";
+ document.getElementById("activenav").style.backgroundColor = "palevioletred";
+  }
+  if(X===7) {
+ document.getElementById("footer").style.backgroundColor = "blanchedalmond";
+ document.getElementById("activenav").style.backgroundColor = "blanchedalmond";
+  }
+  if(X===8) {
+ document.getElementById("footer").style.backgroundColor = "darkslategray";
+ document.getElementById("activenav").style.backgroundColor = "darkslategray";
+  }
+  if(X===9) {
+ document.getElementById("footer").style.backgroundColor = "cornsilk";
+ document.getElementById("activenav").style.backgroundColor = "cornsilk";
+  }
+  if(X<1 || X>9) {
+  document.getElementById("footer").style.backgroundColor = "aquamarine";
+ document.getElementById("activenav").style.backgroundColor = "aquamarine";
+  }
