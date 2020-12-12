e7b279f0dfc5439831de7cfbd20c994c38906bbc
Burksson <57063539+Burksson@users.noreply.github.com>
2020-12-12 13:12:06 +0100

    Update random.js

--- random.js
+++ random.js
@@ -1,3 +1,4 @@
+//footer activenav
 var X = Math.floor(Math.random() * 10); 
   if(X===1) {
      document.getElementById("footer").style.backgroundColor = "red";
@@ -38,4 +39,50 @@ var X = Math.floor(Math.random() * 10);
   if(X<1 || X>9) {
   document.getElementById("footer").style.backgroundColor = "aquamarine";
  document.getElementById("activenav").style.backgroundColor = "aquamarine";
+  }
+
+
+
+
+//bg-linje och fullscreen button (full)
+var bg = Math.floor(Math.random() * 10); 
+  if(bg===1) {
+     document.getElementById("bg-linje").style.backgroundColor = "red";
+    document.getElementById("full").style.backgroundColor = "red";
+     }
+  if(bg===2) {
+  document.getElementById("bg-linje").style.backgroundColor = "teal";
+  document.getElementById("full").style.backgroundColor = "teal";
+  }
+  if(bg===3) {
+ document.getElementById("bg-linje").style.backgroundColor = "orange";
+ document.getElementById("full").style.backgroundColor = "orange";
+  }
+  if(bg===4) {
+ document.getElementById("bg-linje").style.backgroundColor = "steelblue";
+  document.getElementById("full").style.backgroundColor = "steelblue";
+  }
+  if(bg===5) {
+ document.getElementById("bg-linje").style.backgroundColor = "pink";
+ document.getElementById("full").style.backgroundColor = "pink";
+  }
+  if(bg===6) {
+ document.getElementById("bg-linje").style.backgroundColor = "palevioletred";
+ document.getElementById("full").style.backgroundColor = "palevioletred";
+  }
+  if(bg===7) {
+ document.getElementById("bg-linje").style.backgroundColor = "blanchedalmond";
+ document.getElementById("full").style.backgroundColor = "blanchedalmond";
+  }
+  if(bg===8) {
+ document.getElementById("bg-linje").style.backgroundColor = "darkslategray";
+ document.getElementById("full").style.backgroundColor = "darkslategray";
+  }
+  if(bg===9) {
+ document.getElementById("bg-linje").style.backgroundColor = "cornsilk";
+ document.getElementById("full").style.backgroundColor = "cornsilk";
+  }
+  if(bg<1 || bg>9) {
+  document.getElementById("bg-linje").style.backgroundColor = "aquamarine";
+  document.getElementById("full").style.backgroundColor = "aquamarine";
   }
