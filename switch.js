ffd2238b68c428c7791985878139686617ac5e98
Oliver Wigren <57063539+oliverwigren@users.noreply.github.com>
2024-03-29 21:46:55 +0100

    change to switch.js

--- switch.js
+++ switch.js
@@ -3,13 +3,17 @@ const button = document.getElementById("c-button");
 const eduDiv = document.getElementById("education");
 const workDiv = document.getElementById("work");
 
-const switchDiv = () => {
-  if ((eduDiv.style.display = "none")) {
+let bool = true;
+
+const switchDiv = (bool) => {
+  if (bool) {
+    eduDiv.style.display = "none";
+    workDiv.style.display = "block";
+    bool = false;
+  } else {
     eduDiv.style.display = "block";
     workDiv.style.display = "none";
-  } else {
-    workDiv.style.display = "block";
-    eduDiv.style.display = "none";
+    bool = true;
   }
 };
 
